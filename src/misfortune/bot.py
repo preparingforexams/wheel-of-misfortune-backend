83abb4cf2a131cf7652dde066f4a3da3d2809fb1
Björn Petersen <git@bjoernpetersen.net>
2022-06-09 19:32:48 +0200

    Run bot

--- src/misfortune/bot.py
+++ src/misfortune/bot.py
@@ -1,9 +1,25 @@
-import time
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, Filters
+
+from misfortune.config import Config
+
+
+def list_drinks(update: Update, _):
+    pass
 
 
 def run():
-    print("Nah")
-    time.sleep(3600)
+    config = Config.from_env()
+    token = config.telegram_token
+    updater = Updater(token)
+    updater.dispatcher.add_handler(CommandHandler(
+        "list",
+        list_drinks,
+        filters=~Filters.update.edited_message
+    ))
+    updater.start_polling()
+    print("Running")
+    updater.idle()
 
 
 if __name__ == '__main__':
--- src/misfortune/config.py
+++ src/misfortune/config.py
@@ -7,11 +7,13 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     internal_token: str
+    telegram_token: str
     wheel_token: str
 
     @classmethod
     def from_env(cls) -> Config:
         return cls(
             internal_token=os.getenv("INTERNAL_TOKEN"),
+            telegram_token=os.getenv("TELEGRAM_TOKEN"),
             wheel_token=os.getenv("WHEEL_TOKEN"),
         )
