a43a6f79370d12b9fa5ac5bd6d4a1b1fd382d893
Björn Petersen <git@bjoernpetersen.net>
2024-10-07 14:57:04 +0200

    Use bs-config

--- src/misfortune/config.py
+++ src/misfortune/config.py
@@ -1,29 +1,9 @@
 import logging
-import os
 from dataclasses import dataclass
-from typing import Any, Self
+from typing import Self
 
 import sentry_sdk
-
-
-def _non_probe_sampler(context: dict[str, Any]) -> float:
-    transaction = context["transaction_context"]
-    path = transaction.get("path")
-    if (
-        transaction["op"] == "http.server"
-        and path is not None
-        and path.startswith("/probe/")
-    ):
-        return 0.0
-
-    return 1.0
-
-
-def _get_env(key: str) -> str:
-    value = os.getenv(key)
-    if not value:
-        raise ValueError(f"Missing environment variable: {key}")
-    return value
+from bs_config import Env
 
 
 @dataclass
@@ -37,15 +17,18 @@ class Config:
     sentry_dsn: str | None
 
     @classmethod
-    def from_env(cls) -> Self:
+    def from_env(cls, env: Env) -> Self:
         return cls(
-            api_url=os.getenv("API_URL", "https://api.bembel.party"),
-            app_version=os.getenv("APP_VERSION") or "dev",
-            drinks_collection=os.getenv("FIRESTORE_DRINKS_COLLECTION") or "drinks",
-            internal_token=_get_env("INTERNAL_TOKEN"),
-            sentry_dsn=os.getenv("SENTRY_DSN"),
-            telegram_token=_get_env("TELEGRAM_TOKEN"),
-            wheel_token=_get_env("WHEEL_TOKEN"),
+            api_url=env.get_string("API_URL", default="https://api.bembel.party"),
+            app_version=env.get_string("APP_VERSION", default="dev"),
+            drinks_collection=env.get_string(
+                "FIRESTORE_DRINKS_COLLECTION",
+                default="drinks",
+            ),
+            internal_token=env.get_string("INTERNAL_TOKEN", required=True),
+            sentry_dsn=env.get_string("SENTRY_DSN"),
+            telegram_token=env.get_string("TELEGRAM_TOKEN", required=True),
+            wheel_token=env.get_string("WHEEL_TOKEN", required=True),
         )
 
     def basic_setup(self) -> None:
@@ -56,11 +39,11 @@ class Config:
             sentry_sdk.init(
                 dsn=dsn,
                 release=self.app_version,
-                traces_sampler=_non_probe_sampler,
             )
 
 
 def init_config() -> Config:
-    config = Config.from_env()
+    env = Env.load(include_default_dotenv=True)
+    config = Config.from_env(env)
     config.basic_setup()
     return config
