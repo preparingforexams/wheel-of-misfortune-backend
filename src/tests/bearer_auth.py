09731f97ad4c12b447398397c462e6a1631c86a0
Björn Petersen <git@bjoernpetersen.net>
2025-01-03 15:45:15 +0100

    Fix ruff issue

--- src/tests/bearer_auth.py
+++ src/tests/bearer_auth.py
@@ -1,4 +1,4 @@
-import typing
+from collections.abc import Generator
 
 import httpx
 from httpx import Request, Response
@@ -8,6 +8,6 @@ class BearerAuth(httpx.Auth):
     def __init__(self, token: str):
         self._token = token
 
-    def auth_flow(self, request: Request) -> typing.Generator[Request, Response]:
+    def auth_flow(self, request: Request) -> Generator[Request, Response]:
         request.headers["Authorization"] = f"Bearer {self._token}"
         yield request
